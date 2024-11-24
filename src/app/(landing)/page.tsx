1803b2a4e95bcd1811827e58a674295687304907
Jack <jackrgisel@gmail.com>
2024-11-24 14:12:32 -0800

    fix

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -204,21 +204,21 @@ export default function HomePage() {
             </div>
 
             {/* 4th bento grid */}
-            <div className="relative lg:grow-span-2">
+            <div className="relative lg:row-span-2">
               <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
 
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-                <div className="px-8 pb-8 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                   <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
-                    Easy integration
+                    Easy Integration
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Connect PingaPanda with your existing workflows in minutes
+                    Connect PingPanda with your existing workflows in minutes
                     and call our intuitive logging API from any language.
                   </p>
                 </div>
 
-                <div className="relative m-h-[30rem] w-full grow">
+                <div className="relative min-h-[30rem] w-full grow">
                   <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
                     <div className="flex bg-gray-800/40 ring-1 ring-white/5">
                       <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
@@ -230,6 +230,8 @@ export default function HomePage() {
                   </div>
                 </div>
               </div>
+
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
             </div>
           </div>
         </MaxWidthWrapper>
