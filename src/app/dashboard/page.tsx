81f41a66a9f0a99a18875ca7e211ea8161f991dc
Jack <jackrgisel@gmail.com>
2024-12-01 07:47:43 -0800

    sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -30,7 +30,7 @@ const Page = async () => {
       hideBackButton
       cta={
         <CreateEventCategoryModal>
-          <Button>
+          <Button className="w-full sm:w-fit">
             <PlusIcon className="size-4 mr-2 " />
             Add Category
           </Button>
--- src/components/dashboard-page.tsx
+++ src/components/dashboard-page.tsx
@@ -19,15 +19,17 @@ export const DashboardPage = ({
   return (
     <section className="flex-1 h-full w-full flex flex-col">
       <div className="p-6 sm:p-8 flex justify-between border-b border-gray-200">
-        <div className="flex flex-col sm:flex-row sm:items-center gap-y-2- gap-x-8">
-          {hideBackButton ? null : (
-            <Button className="w-fit bg-white" variant="outline">
-              <ArrowLeft className="size-4" />
-            </Button>
-          )}
-          <Heading className="text-md">{title}</Heading>
+        <div className="w-full flex flex-col sm:flex-row items-start sm:items-center gap-6">
+          <div className="flex items-center gap-8">
+            {hideBackButton ? null : (
+              <Button className="w-fit bg-white" variant="outline">
+                <ArrowLeft className="size-4" />
+              </Button>
+            )}
+            <Heading className="text-md">{title}</Heading>
+          </div>
 
-          {cta ? <div>{cta}</div> : null}
+          {cta ? <div className="w-full">{cta}</div> : null}
         </div>
       </div>
 
