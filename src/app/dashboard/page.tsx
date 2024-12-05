8879d25c28a51eed6b7c5b24ad6d949920d3ff9c
Jack <jackrgisel@gmail.com>
2024-12-05 07:54:35 -0800

    bug fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -41,7 +41,7 @@ const Page = async ({ searchParams }: PageProps) => {
     if (session.url) redirect(session.url)
   }
 
-  const success = searchParams.sucess
+  const success = searchParams.success
 
   return (
     <>
