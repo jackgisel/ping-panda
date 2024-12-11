098f09dbe3d0f6aad8d01d65db59c097e258fcaa
Jack <jackrgisel@gmail.com>
2024-12-11 07:02:37 -0800

    remove line

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,5 +1,3 @@
 import { httpHandler } from "@/server"
 
-export { /* @next-codemod-error `httpHandler` export is re-exported. Check if this component uses `params` or `searchParams`*/
-httpHandler as GET, /* @next-codemod-error `httpHandler` export is re-exported. Check if this component uses `params` or `searchParams`*/
-httpHandler as POST }
+export { httpHandler as GET, httpHandler as POST }
